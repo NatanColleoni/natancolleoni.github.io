089291ba91ccfb991c0f8b414353c8ac6c8a2e04
NatanColleoni <62671225+NatanColleoni@users.noreply.github.com>
2020-11-04 11:39:40 -0300

    Add files via upload

---
+++ projects/bikcraft/js/script.js
@@ -0,0 +1,18 @@
+if(window.SimpleSlide) {
+    new SimpleSlide ({
+     slide: "quote", 
+     auto: true, 
+      time: 5000 // tempo de transição
+});
+
+    new SimpleSlide ({
+     slide: "portfolio", 
+     auto: true, 
+      nav: true,
+      time: 5000 // tempo de transição
+});
+}
+
+if (window.SimpleAnime){
+    new SimpleAnime();
+}
